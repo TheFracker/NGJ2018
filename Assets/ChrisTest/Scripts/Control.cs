e91b9352d982b1a054b8780f6a6322a667998b18
Christopher Bech <Christopher@alloff.dk>
2018-04-14 00:31:39 +0200

    2nd player

--- Assets/ChrisTest/Scripts/Control.cs
+++ Assets/ChrisTest/Scripts/Control.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -32,38 +32,4 @@ public class Control : MonoBehaviour {
         rb2d.AddForce (movement * speed);
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class Control : MonoBehaviour {
-
-    public float speed;             //Floating point variable to store the player's movement speed.
-
-    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
-
-    // Use this for initialization
-    void Start()
-    {
-        //Get and store a reference to the Rigidbody2D component so that we can access it.
-        rb2d = GetComponent<Rigidbody2D> ();
-    }
-
-    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
-    void FixedUpdate()
-    {
-        //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = 0;
-
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce (movement * speed);
-    }
-}
->>>>>>> 7c363cb12a3db079d8de29b4b2067aab6c68a75e
