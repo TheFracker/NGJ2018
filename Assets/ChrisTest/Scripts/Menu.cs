06481545c95da53ddd96196b995b8b5587b0c53b
Christopher Bech <Christopher@alloff.dk>
2018-04-14 17:56:44 +0200

    Menu update

---
+++ Assets/ChrisTest/Scripts/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    bool onStart;
+    bool onQuit;
+    bool gameStarting;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        while (gameStarting == false)
+        {
+
+            if (Input.GetAxis("Vertical") < 0)
+            {
+                print("going up");
+                //print(Input.GetAxis("Vertical"));
+            }
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                print("going down");
+                //print(Input.GetAxis("Vertical"));
+            }
+        }
+		
+	}
+}
