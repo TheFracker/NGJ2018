b59f51e7f0258464cd5572ee324c094db44e55b2
skarregaard89 <skarregaard89@gmail.com>
2018-04-14 14:13:51 +0200

    holes

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    private static int numberOfBrokenStuff;
+
+    [SerializeField]
+    private List<GameObject> thingsThatBreakStuff;
+
+
+    float timeStamp;
+
+	// Use this for initialization
+	void Start () {
+        thingsThatBreakStuff[0].GetComponent<IBreakable>().Break();
+        timeStamp = Time.time;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(Time.time - timeStamp);
+
+		
+	}
+}
