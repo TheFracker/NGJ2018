acd6efc38c7c28208010a43464ab2525a66a1b91
Anders Olsen <mr.a.olsen@gmail.com>
2018-04-14 11:31:36 +0200

    Gauge is no longer a MonoBehaviour

--- Assets/Scripts/Gauge.cs
+++ Assets/Scripts/Gauge.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gauge : MonoBehaviour, IGauge
+public class Gauge : IGauge
 {
     public int FailureCounter { get; private set; } = 0;
 
