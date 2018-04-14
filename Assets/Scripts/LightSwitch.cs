05a1e16400e12944c33cb5ce08198981cd0166c6
Anders Olsen <mr.a.olsen@gmail.com>
2018-04-14 10:39:39 +0200

    Added script for light switches

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour, IBreakable
+{
+    [SerializeField]
+    private List<Light> lights = new List<Light>();
+
+    void Awake()
+    {
+        foreach (var light in lights)
+        {
+            light.enabled = true;
+        }
+    }
+
+    public void Break()
+    {
+        foreach (var light in lights)
+        {
+            light.enabled = false;
+        }
+        Locator.GetGauge().Add();
+    }
+}
