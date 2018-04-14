6f30ded9a34388207379ac169f2c4865f490412e
Nikolaj Thomsen <nikolaj.thomsen@gmail.com>
2018-04-14 13:20:50 +0200

    Doors can now break

---
+++ Assets/NikolajTest/Scripts/Door.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public SpriteRenderer thisSpriteRenderer;
+    public Sprite doorOpen;
+    public Sprite doorClosed;
+    private DoorCollider _collider;
+
+    // Use this for initialization
+    void Start()
+    {
+        thisSpriteRenderer = GetComponent<SpriteRenderer>();
+        _collider = GetComponentInChildren<DoorCollider>();
+    }
+
+    void Update()
+    {
+        if (_collider.open)
+        {
+            thisSpriteRenderer.sprite = doorOpen;
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+        else if (!_collider.open)
+        {
+            thisSpriteRenderer.sprite = doorClosed;
+            GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+}
---
+++ Assets/NikolajTest/Scripts/DoorSwitch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSwitch : MonoBehaviour
+{
+    public List<DoorCollider> doors = new List<DoorCollider>();
+    public bool doorsBroken = false;
+
+    // Use this for initialization
+    void Start()
+    {
+         doorsBroken = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2") && doorsBroken == false)
+        {
+            DoorBreak();
+        }
+
+       /*if (Input.GetButtonDown("Fire2") && doorsBroken == true)
+        {
+            DoorRepair();
+        }*/
+
+    }
+
+    void DoorBreak()
+    {
+        print("doors broken");
+        foreach (var door in doors)
+        {
+            door.broken = true;
+        }
+        doorsBroken = true;
+    }
+
+    void DoorRepair()
+    {
+        print("doors repaired");
+        foreach (var door in doors)
+        {
+            door.broken = false;
+        }
+        doorsBroken = false;
+
+    }
+
+}
