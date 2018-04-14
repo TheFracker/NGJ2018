a702511327b7ad480f17b4705fa6204b21d1dab0
Anders Olsen <mr.a.olsen@gmail.com>
2018-04-14 19:38:36 +0200

    Deleted unused and unneeded scripts. Renamed GrabberScriptPlayerOne to PlayerGrabber and refactored

---
+++ Assets/ChrisTest/Scripts/PlayerGrabber.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGrabber : MonoBehaviour
+{
+    public bool Grabbed { get; private set; }
+
+    RaycastHit2D _hit;
+    public Transform HoldPoint;
+    public Transform OtherConsolePoint;
+    public float Distance = 2f;
+    public bool PlayerOnConsol;
+    public GameObject Consol;
+    public GameObject Kit;
+    public LayerMask Mask = 8;
+    public int PlayerNumber { get; private set; }
+
+    void Start()
+    {
+        PlayerNumber = int.Parse(transform.name.Remove(0, transform.name.Length - 1));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown($"Player{PlayerNumber}Inter1"))
+        {
+            if (!Grabbed)
+            {
+                Physics2D.queriesStartInColliders = false;
+                _hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, Distance, Mask.value);
+
+                if (_hit.collider != null)
+                {
+                    Grabbed = true;
+                    GetComponent<Animator>().SetBool("carringStarFish", Grabbed);
+                }
+                //grab
+            }
+            else if (_hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                //throw
+                Grabbed = false;
+                GetComponent<Animator>().SetBool("carringStarFish", Grabbed);
+
+                if (PlayerOnConsol)
+                {
+                    _hit.collider.gameObject.SetActive(true);
+                    _hit.collider.gameObject.transform.position = new Vector2(OtherConsolePoint.position.x, OtherConsolePoint.position.y);
+                }
+                else
+                {
+                    _hit.collider.gameObject.SetActive(true);
+                    _hit.collider.gameObject.transform.position = new Vector2(this.transform.position.x, this.transform.position.y);
+                }
+
+                _hit.collider.gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+            }
+        }
+
+        if (Grabbed && _hit.collider != null)
+        {
+            _hit.collider.gameObject.SetActive(false);
+            _hit.collider.gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject == Consol)
+        {
+            PlayerOnConsol = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        PlayerOnConsol = false;
+    }
+}
