16199634067cc3b878d77bad3e4a607156224780
Christopher Bech <Christopher@alloff.dk>
2018-04-15 12:35:40 +0200

    gameOverFinal

---
+++ Assets/gameOverMoviePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameOverMoviePlayer : MonoBehaviour
+{
+
+    public MovieTexture movie;
+    RawImage rawImageComp;
+
+    // Use this for initialization
+    void Start()
+    {
+        rawImageComp = GetComponent<RawImage>();
+        playGameOver();
+
+    }
+
+    void playGameOver()
+    {
+        rawImageComp.texture = movie;
+        movie.Play();
+    }
+}
